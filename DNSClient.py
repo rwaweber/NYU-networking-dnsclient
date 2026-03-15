a41916b6b8fb7c6c243f8df613a3959780640ee6
rwaweber <rwaweber@chump.info>
2026-03-15 16:08:51 -0400

    [query functions] use rrset attributes

--- DNSClient.py
+++ DNSClient.py
@@ -1,8 +1,8 @@
 import dns.resolver
 
 # Set the IP address of the local DNS server and a public DNS server
-local_host_ip = ????
-real_name_server = ???? # Research public DNS servers to find a valid DNS server IP address to use
+local_host_ip = "127.0.0.1"
+real_name_server = "1.1.1.1" # cloudflare public dns
 
 
 # Create a list of domain names to query - use the same list from the DNS Server
@@ -11,19 +11,19 @@ domainList  = ['example.com.','safebank.com.','google.com.','nyu.edu.','legitsit
 # Define a function to query the local DNS server for the IP address of a given domain name
 def query_local_dns_server(domain,question_type):
     resolver = dns.resolver.Resolver()
-    resolver.nameservers = [????]
-    answers = resolver.resolve(????, ????) # provide the domain and question_type
+    resolver.nameservers = [local_host_ip]
+    answers = resolver.resolve(domain, question_type) # provide the domain and question_type
 
-    ip_address = answers[??].to_text()
+    ip_address = answers.rrset.to_text()
     return ip_address
 
 # Define a function to query a public DNS server for the IP address of a given domain name
 def query_dns_server(domain,question_type):
     resolver = dns.resolver.Resolver()
-    resolver.nameservers = [????]
+    resolver.nameservers = [real_name_server]
     answers = resolver.resolve(domain, question_type) # provide the domain and question_type
 
-    ip_address = answers[??].to_text()
+    ip_address = answers.rrset.to_text()
     return ip_address
 
 # Define a function to compare the results from the local and public DNS servers for each domain name in the list
